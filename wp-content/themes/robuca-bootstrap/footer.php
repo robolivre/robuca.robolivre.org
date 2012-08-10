ab39e013363c644fcc512eb56e131a218a11e991
Rodrigo Muniz <eu@rodrigomuniz.com>
2012-08-10 15:39:57 -0300

    Google Analytics

--- wp-content/themes/robuca-bootstrap/footer.php
+++ wp-content/themes/robuca-bootstrap/footer.php
@@ -40,10 +40,24 @@
 			</div><!-- #page -->
 		</div><!-- .container -->
 
-	<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'></script>
-	<script type='text/javascript' src='<?php bloginfo('stylesheet_directory'); ?>/bootstrap/js/bootstrap.min.js'></script>
-	<script type='text/javascript' src='<?php bloginfo('stylesheet_directory'); ?>/js/main-robuca.js'></script>
+	<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'></script>
+	<script src='<?php bloginfo('stylesheet_directory'); ?>/bootstrap/js/bootstrap.min.js'></script>
+	<script src='<?php bloginfo('stylesheet_directory'); ?>/js/main-robuca.js'></script>
 
 	<?php wp_footer(); ?>
+
+	<script>
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-24423908-2']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
+
 	</body>
 </html>
